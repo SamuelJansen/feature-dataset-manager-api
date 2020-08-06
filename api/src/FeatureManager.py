0506b208d72c0f09cae0206d6467665d2f4f961b
samuel.jansen <samuel.jansenn@gmail.com>
2020-08-06 00:40:34 -0300

    feat: local database removed

--- api/src/FeatureManager.py
+++ api/src/FeatureManager.py
@@ -4,7 +4,7 @@ import ModelAssociation
 api, app, jwt = ResourceManager.initialize(
     __name__,
     ModelAssociation.Model,
-    # databaseEnvironmentVariable = 'DATABASE_URL',
-    jwtSecret = '3s7tj83ry17**q837yrk1v3r7k32vdg781dkd73u',
-    localStorageName = 'LocalFeatureManager'
+    databaseEnvironmentVariable = 'DATABASE_URL',
+    jwtSecret = '3s7tj83ry17**q837yrk1v3r7k32vdg781dkd73u'
+    # localStorageName = 'LocalFeatureManager'
 )
