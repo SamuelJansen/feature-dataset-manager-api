495263efd81023604ca043ef24309fa079279d4e
samuel.jansen <samuel.jansenn@gmail.com>
2020-08-23 22:21:24 -0300

    feat: host and schemes refference method update

--- api/src/FeatureManager.py
+++ api/src/FeatureManager.py
@@ -6,6 +6,6 @@ api, app, jwt = ResourceManager.initialize(
     ModelAssociation.Model,
     baseUrl = '/september',
     jwtSecret = '3s7tj83ry17**q837yrk1v3r7k32vdg781dkd73u',
-    # databaseEnvironmentVariable = 'DATABASE_URL'
-    localStorageName = 'LocalFeatureManager'
+    databaseEnvironmentVariable = 'DATABASE_URL'
+    # localStorageName = 'LocalFeatureManager'
 )
