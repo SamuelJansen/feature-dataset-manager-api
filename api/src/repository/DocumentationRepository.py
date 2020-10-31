16a27db39687a7fc33b6bdd1b9c45690d0a9fd1b
samuel.jansen <samuel.jansenn@gmail.com>
2020-10-31 17:51:54 -0300

    feat: python_framework updated

--- api/src/repository/DocumentationRepository.py
+++ api/src/repository/DocumentationRepository.py
@@ -4,4 +4,6 @@ from python_framework import Repository, OpenApiDocumentationFile
 class DocumentationRepository:
 
     def getSwaggerDocumentation(self):
+        import sys
+        sys.path.append(self.globals.api.documentationFolderPath)
         return OpenApiDocumentationFile.loadDocumentation(self.globals.api)
