5155b5eda9b3e8100011eaad1a4cba1feeb85c55
samuel.jansen <samuel.jansenn@gmail.com>
2020-11-13 05:49:38 -0300

    feat: static files package updated

--- api/src/controller/ActuatorHealthController.py
+++ api/src/controller/ActuatorHealthController.py
@@ -1,7 +1,14 @@
 from python_framework import Controller, ControllerMethod, HttpStatus
 
 @Controller(url='/actuator/health', tag='HealthCheck', description='HealthCheck controller')
-class ActuatorHealthCheckController:
+class ActuatorHealthController:
+
+    @ControllerMethod()
+    def get(self):
+        return {'status' : 'UP'}, 200
+
+@Controller(url='/', tag='HealthCheck', description='HealthCheck controller')
+class ActuatorHealthBatchController:
 
     @ControllerMethod()
     def get(self):
