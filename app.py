559f1068564c787d480b8fbc4c8a08639ea7781a
samuel.jansen <samuel.jansenn@gmail.com>
2021-11-07 06:04:51 -0300

    refact: framework bug fixed

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 from python_framework import newApp, runApi
-app = newApp(__file__,
-    debugStatus = True
-    , warningStatus = True
-    , wrapperStatus = True
-    , testStatus = True
-    , logStatus = True
+app = newApp(__file__
+    # , debugStatus = True
+    # , warningStatus = True
+    # , wrapperStatus = True
+    # , testStatus = True
+    # , logStatus = True
 )
-import globals
-globalsInstance = globals.getGlobalsInstance()
-print(globalsInstance.apiName)
-
-
-print(type(app))
 if __name__ == '__main__':
     runApi()
