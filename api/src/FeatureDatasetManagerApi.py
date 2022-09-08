7813bd7b9e205f432e7fd4ac0942717f1e2e1656
samuel.jansen <samuel.jansenn@gmail.com>
2022-09-08 15:30:39 -0300

    feature: configurations

--- api/src/FeatureDatasetManagerApi.py
+++ api/src/FeatureDatasetManagerApi.py
@@ -5,14 +5,3 @@ app = ResourceManager.initialize(
     __name__,
     ModelAssociation.Model
 )
-
-import os
-from flask import send_from_directory
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(
-        os.path.join(app.root_path, 'static'),
-        'favicon.ico',
-        mimetype='image/vnd.microsoft.icon'
-    )
